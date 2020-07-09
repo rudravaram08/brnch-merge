d9627785b033aba5b2134f3a655c397ac605ae98
unknown <rpramodh811@gmail.com>
2020-07-09 14:08:13 +0530

    liq

---
+++ hungrydup.java
@@ -0,0 +1,13 @@
+
+public class hungrydup {
+
+	public static void main(String[] args) {
+
+	System.out.println("pizza");
+	System.out.println("burger");
+	System.out.println("roles");
+	System.out.println("fries");
+	
+	}
+
+}
