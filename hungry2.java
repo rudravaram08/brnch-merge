a0fad46ee3fff3690405e51e706016e6d2e7b1b3
unknown <rpramodh811@gmail.com>
2020-07-09 14:26:29 +0530

    w

---
+++ hungry2.java
@@ -0,0 +1,15 @@
+
+public class hungry2 {
+
+	public static void main(String[] args) {
+
+	System.out.println("pizza");
+	System.out.println("burger");
+	System.out.println("roles");
+	System.out.println("water");
+	System.out.println("soda");
+	
+	
+	}
+
+}
